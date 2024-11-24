4d2a11c24dc278090f649887c9ebdec39b8dbfdb
dylululu <duynhatluu2003@gmail.com>
2024-11-24 20:21:37 +0700

    m

--- Script/Controls.js
+++ Script/Controls.js
@@ -4,12 +4,12 @@ audioStartsAt("TBN", 2);
 
 audioStartsAt("Trong", 1);
 
-audioStartsAt("AP", 53);
+audioStartsAt("AP", 54);
 
 tangDan("AP", 0,1,0.1,0.6);
 
 giamTangDan("TT",1,0,0.1,0.6,153,153);
 
-giamDanDenGiay("Sao",1,0,0.1,0.5,40);
+giamDanDenGiay("Sao",1,0,0.1,0.5,34);
 
 
